144f3d2284211fe2b3c78576d4bf9edab04d32ef
Chris <accounts@chrisadams.io>
2024-03-29 23:24:32 -0400

    CI: package update

--- tend.js
+++ tend.js
@@ -4,14 +4,12 @@ function generatePlaceholder(width, height, backgroundColor, textColor, text) {
   return new Promise((resolve, reject) => {
     new Jimp(width, height, backgroundColor, (err, image) => {
       if (err) reject(err);
-
       // Add text (if provided)
       if (text) {
         Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
           image.print(font, 10, 10, text);
         });
       }
-
       // Return as base64 data URI  
       image.getBase64(Jimp.MIME_PNG, (err, dataUri) => {
         if (err) reject(err);
